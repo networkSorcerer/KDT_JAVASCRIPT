e37d3ae0ea596dae9f976e137c8a23060f418306
networkSorcerer <gigattfyhig@gmail.com>
2024-11-12 17:24:45 +0900

    String

--- 객체.js
+++ 객체.js
@@ -16,3 +16,81 @@ console.log(person.name);
 console.log(person["age"]);
 console.log(person);
 console.log(person.info());
+
+const profile = [
+  {
+    name: "안유진",
+    age: 21,
+    history: [
+      {
+        date: "2022-09-01",
+        product: "iPhone14 Pro",
+      },
+      {
+        date: "2023-12-01",
+        product: "iPhone15 Pro Max",
+      },
+    ],
+  },
+  {
+    name: "장원영",
+    age: 20,
+    history: [
+      {
+        date: "2024-08-05",
+        product: "iPhone16 Pro",
+      },
+    ],
+  },
+  {
+    name: "", // 나머지 값은 생략 가능
+  },
+];
+
+// 객체 속성 동적 추가하기
+// 객체 속성에 키로 접근해 값을 재할당하면 기존 속성값을 변경할수 있음
+// 키가 없는 경우 , 키와값으로 구성된 새로운 속성이 객체로 추가
+const carInfo = {};
+carInfo.name = "BMW X6";
+carInfo.year = "2023/04/15";
+carInfo.maxSpeed = "250km";
+
+delete carInfo.maxSpeed;
+console.log(carInfo);
+
+// 생성자 : 자바스크립트에서는 클래스 대신 함수를 생성자를 통해서 객체로 만들수 있음
+function Person(name, age, addr) {
+  this.name = name;
+  this.age = age;
+  this.addr = addr;
+  this.info = function () {
+    return `이름 : ${this.name}, 나이 : ${this.age}, 주소 : ${this.addr}`;
+  };
+}
+
+// const person1 = new Person("안유진", 21, "서울시 강남구 역삼동 ");
+// const person2 = new Person("장원영", 20, "서울시 강남구 청담동");
+// const person3 = new Person("이서", 18, "서울시 강남구 삼성동");
+
+// console.log(person1.info());
+// console.log(person2.info());
+// console.log(person3.info());
+
+// 클래스 방식
+class Person {
+  constructor(name, age, addr) {
+    this.name = name;
+    this.age = age;
+    this.addr = addr;
+  }
+  info() {
+    return `이름 : ${this.name}, 나이 : ${this.age}, 주소 : ${this.addr}`;
+  }
+}
+const person1 = new Person("안유진", 21, "서울시 강남구 역삼동 ");
+const person2 = new Person("장원영", 20, "서울시 강남구 청담동");
+const person3 = new Person("이서", 18, "서울시 강남구 삼성동");
+
+console.log(person1.info());
+console.log(person2.info());
+console.log(person3.info());
---
+++ 스트링.js
@@ -0,0 +1,35 @@
+//문자열을 다루는 string 객체
+const pwd = "12345678";
+console.log(pwd.length); // 문자열의 길이 확인 length
+
+// 특정 문자열의 포함여부 확인 : include("문자열"), indexOf("문자열")
+// include("문자열" ) : 포함여부를 true/false
+// indexOf("문자열" ) : 시작 인덱스를 반환하고 없으면 -1
+const email = "test!naver.com";
+if (!email.includes("@")) {
+  console.log("올바른 이메일 형식이 아닙니다.");
+}
+
+const email2 = "test!naver.com";
+if (email2.indexOf("@") == -1) {
+  console.log("올바른 이메일 형식이 아닙니다.");
+}
+
+// lastIndexOf() : 찾고자 하는 문자열이 둘 이상 발견되면 제일 마지막에 발견된 문자열의  index반환
+const email3 = "test@naver@com";
+console.log(email3.lastIndexOf("@"));
+
+// slice(): 시작위치와 종료 위치를 주면 해당 문자열을 잘라내서 반환 음수값을 줄수 있음
+const fruits = "Apple, Banana, Orange";
+const sliceVal = fruits.slice(-6); //  시작 인덱스, 종료 인텍스 미만 개념
+console.log(sliceVal);
+
+// substring() : 시작 위치와 종료 위치를 주면 해당 문자열을 잘라내서 반환
+const fruits2 = "Apple, Banana, Orange";
+const sliceVal2 = fruits2.substring(7, 13); //  시작 인덱스, 종료 인텍스 미만 개념
+console.log(sliceVal2);
+
+//replace() : 문자열 내의 문자열을 지정한 문자열로 바꾸는 함수
+const names = "안유진, 이영지, 미미, 이은지, 나영석";
+const newNames = names.replace("나영석", "장원영");
+console.log(newNames);
